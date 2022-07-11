9a5c9982892226ccd536a2845cd453929b53c871
xin wang <1804933537@qq.com>
2022-07-11 23:44:51 +0800

    add

--- class_test/第二天/work1.c
+++ class_test/第二天/work1.c
@@ -6,36 +6,52 @@ int main(int argc,const char* argv[])
 					 {6,8,10,6,5}};
 	int m = 0, n = 0;
 
-	for(int i=0; i<5;i++)
+```c
+for(int i=0; i<5;i++)
+{
+	for(int j=0;j<5;j++)
 	{
-		for(int j=0;j<5;j++)
+		if(arr[i][j]<arr[m][n])
 		{
-			if(arr[i][j]<arr[m][n])
-			{
-				m = i;
-				n = j;
-			}
+			m = i;
+			n = j;
 		}
 	}
-	int sum =0;
-	printf("min num = %d\n", arr[m][n]);
-	if(m-1>-1)
-		sum +=arr[m-1][n];
-	if(m+1<5)
-		sum +=arr[m+1][n];
-	if(n-1>-1)
-		sum +=arr[m][n-1];
-	if(n+1<5)
-		sum +=arr[m][n+1];
-	if(m-1>-1&&n-1>-1)
-		sum +=arr[m-1][n-1];
-	if(m-1>-1&&n+1<5)
-		sum +=arr[m-1][n+1];
-	if(m+1<5&&n-1>-1)
-		sum +=arr[m+1][n-1];
-	if(m+1<5&&n+1<5)
-		sum +=arr[m+1][n+1];
+}
+int sum =0;
+printf("min num = %d\n", arr[m][n]);
+if(m-1>-1)
+	sum +=arr[m-1][n];
+if(m+1<5)
+	sum +=arr[m+1][n];
+if(n-1>-1)
+	sum +=arr[m][n-1];
+if(n+1<5)
+	sum +=arr[m][n+1];
+if(m-1>-1&&n-1>-1)
+	sum +=arr[m-1][n-1];
+if(m-1>-1&&n+1<5)
+	sum +=arr[m-1][n+1];
+if(m+1<5&&n-1>-1)
+	sum +=arr[m+1][n-1];
+if(m+1<5&&n+1<5)
+	sum +=arr[m+1][n+1];
 
-	printf("sum= %d\n",sum);
+printf("sum= %d\n",sum);
+
+//  循环累加
+for(int x = min_x-1; x<=mix_x+1; x++)
+	{
+		for(int y=min_y-1;y<=min_y+1;y++)
+			{
+				if(x>=0&&x<=4&&y>=0&&y<=4)
+				sum  += arr[x][y];	
+			}
+	}
+	printf("%d\n", sum -arr[min_x][min_y]);
+```
+
+
+​	
 	return 0;
 }
---
+++ 学习笔记/C语言32个关键字.c
@@ -0,0 +1,15 @@
+数据类型相关：
+    内建类型：
+        char  short  int  long  void  float  double     7
+    自建类型：
+        struct  union  enum  sizeof                     4
+    类型限定符：
+        auto  const  static volatile register typedef extern signed unsigned                          9
+流程控制相关：
+    分支：
+        if   else    switch  case  default               5
+    循环：
+        for  while  do                                  3
+    跳转：
+        break   continue   goto   return                4
+
